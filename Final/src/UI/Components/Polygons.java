52b320b3b81bb711414db14463a74db49658e4b1
Zolcsák Gergő <zolcsakgergo2001@gmail.com>
2022-05-14 20:07:47 +0200

    polygon centroid calculated

--- Final/src/UI/Components/Polygons.java
+++ Final/src/UI/Components/Polygons.java
@@ -7,15 +7,17 @@ public class Polygons {
    public int[] xValues;
    public int[] yValues;
   public String color;
-   public int middle;
+   public int middleX;
+    public int middleY;
    public int[] neighbours;
 
-    public Polygons(int id, int[] xValues, int[] yValues, String color, int middle, int[] neighbours) {
+    public Polygons(int id, int[] xValues, int[] yValues, String color, int middleX,int middleY, int[] neighbours) {
         this.id = id;
         this.xValues = xValues;
         this.yValues = yValues;
         this.color = color;
-        this.middle = middle;
+        this.middleX = middleX;
+        this.middleY = middleY;
         this.neighbours = neighbours;
     }
 }
